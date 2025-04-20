4daf75b88f4972ef8e5a7a487e612c9ece907320
frtasoniero <frtasoniero@gmail.com>
2025-04-20 19:25:45 -0300

    prova inducao ex1 lemma

--- T1_2025_1.thy
+++ T1_2025_1.thy
@@ -6,9 +6,35 @@ primrec pot :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
 poteq1: "pot x 0 = Suc 0" |
 poteq2: "pot x (Suc y) = x * pot x y"
 
+(* Lema: pot(x, m + n) = pot(x, m) * pot(x, n) *)
 lemma t1: "\<forall>x m::nat. pot x (m + n) = pot x m * pot x n"
-  sorry
+proof (induction n)
+  case 0
+  show "\<forall>x m::nat. pot x (m + 0) = pot x m * pot x 0"
+  proof (rule allI, rule allI)
+    fix A :: nat and B :: nat
+    have "pot A (B + 0) = pot A B" by simp
+    also have "... = pot A B * 1" by simp
+    also have "... = pot A B * pot A 0" by (simp only: poteq1)
+    finally show "pot A (B + 0) = pot A B * pot A 0" .
+  qed
+next
+  case (Suc n)
+  show "\<forall>x m::nat. pot x (m + Suc n) = pot x m * pot x (Suc n)"
+  (*Suc.IH: \<forall>x m. pot x (m + n) = pot x m * pot x n*)
+  proof (rule allI, rule allI)
+    fix A :: nat and B :: nat
+    have "pot A (B + Suc n) = pot A (Suc (B + n))" by simp
+    also have "... = A * pot A (B + n)" by (simp only: poteq2)
+    also have "... = A * (pot A B * pot A n)" using Suc.IH by simp
+    also have "... = (A * pot A n) * pot A B" by simp
+    also have "... = pot A (Suc n) * pot A B" by (simp only: poteq2)
+    also have "... = pot A B * pot A (Suc n)" by simp
+    finally show "pot A (B + Suc n) = pot A B * pot A (Suc n)" .
+  qed
+qed
 
+(* Teorema: pot(x, m * n) = pot(pot(x, m), n) *)
 theorem t2: "\<forall>x m::nat. pot x (m * n) = pot (pot x m) n"
   sorry
 
